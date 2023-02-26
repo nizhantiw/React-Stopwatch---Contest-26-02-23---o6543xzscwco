da1057d7117f37c9d3e7e65f694d079caf885047
nizhantiw <nizhantiw@gmail.com>
2023-02-26 15:54:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,68 @@
 import React, { useRef, useState } from 'react'
 import '../styles/App.css';
+
 const App = () => {
   const startTime = useRef(0);
   const intervalRef = useRef(0);
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
-  
-    const startTimer = () => {
+  const [lapsAdded, setLapsAdded] = useState(false);
+
+  const formatTime = (time) => {
+    const ms = `00${(time % 1000)}`.slice(-3);
+    const s = `0${Math.floor(time / 1000) % 60}`.slice(-2);
+    const m = `0${Math.floor(time / (1000 * 60)) % 60}`.slice(-2);
+    const h = `0${Math.floor(time / (1000 * 60 * 60))}`.slice(-2);
+    return `${h}:${m}:${s}.${ms}`;
+  };
+
+  const handleStart = () => {
     startTime.current = Date.now() - currentTime;
     intervalRef.current = setInterval(() => {
       setCurrentTime(Date.now() - startTime.current);
     }, 10);
   };
 
-  const stopTimer = () => {
+  const handleStop = () => {
     clearInterval(intervalRef.current);
   };
 
-  const lapTimer = () => {
-    const lapTime = currentTime.toFixed(3);
-    setLaps([...laps, lapTime]);
+  const handleLap = () => {
+    const lapTime = formatTime(currentTime);
+    setLaps((laps) => [...laps, lapTime]);
+    setLapsAdded(true);
   };
 
-  const resetTimer = () => {
+  const handleReset = () => {
+    clearInterval(intervalRef.current);
     setCurrentTime(0);
     setLaps([]);
-    clearInterval(intervalRef.current);
+    setLapsAdded(false);
   };
 
   return (
     <div id="main">
-     <section>
-        <h1 className="seconds-elapsed">{(currentTime / 1000).toFixed(3)}</h1>
-        <section className="buttons">
-          <button className="start-btn" onClick={startTimer}>
-            START
-          </button>
-          <button className="stop-btn" onClick={stopTimer}>
-            STOP
-          </button>
-          <button className="lap-btn" onClick={lapTimer}>
-            LAP
-          </button>
-          <button className="reset-btn" onClick={resetTimer}>
-            RESET
-          </button>
+      <section>
+        <h1 className='seconds-elapsed'>{formatTime(currentTime)}</h1>
+        <section className='buttons'>
+          <button className="start-btn" onClick={handleStart}>START</button>
+          <button className="stop-btn" onClick={handleStop}>STOP</button>
+          <button className="lap-btn" onClick={handleLap}>LAP</button>
+          <button className="reset-btn" onClick={handleReset}>RESET</button>
         </section>
       </section>
-      <section className={`lap-section ${laps.length > 0 ? 'visible' : ''}`}>
-        <h2>Laps</h2>
-        {laps.length > 0 ? (
-          <section className="laps">
+      {lapsAdded &&
+        <section className='lap-section'>
+          <h2>Laps</h2>
+          <section className='laps'>
             {laps.map((lap, index) => (
-              <p key={index}>Lap {index + 1}: {lap}s</p>
+              <p key={index}>{`Lap ${index + 1}: ${lap}`}</p>
             ))}
           </section>
-        ) : (
-          <p>No Laps Recorded Yet</p>
-        )}
-      </section>
+        </section>
+      }
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
