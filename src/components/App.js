15cd9f4be7591fa5e9df8cb3218625e20ba4696f
nizhantiw <nizhantiw@gmail.com>
2023-02-26 15:49:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,25 +5,59 @@ const App = () => {
   const intervalRef = useRef(0);
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
+  
+    const startTimer = () => {
+    startTime.current = Date.now() - currentTime;
+    intervalRef.current = setInterval(() => {
+      setCurrentTime(Date.now() - startTime.current);
+    }, 10);
+  };
+
+  const stopTimer = () => {
+    clearInterval(intervalRef.current);
+  };
+
+  const lapTimer = () => {
+    const lapTime = currentTime.toFixed(3);
+    setLaps([...laps, lapTime]);
+  };
+
+  const resetTimer = () => {
+    setCurrentTime(0);
+    setLaps([]);
+    clearInterval(intervalRef.current);
+  };
 
   return (
     <div id="main">
-      <section>
-        <h1 className='seconds-elapsed'>Stopwatch Time</h1>
-        <section className='buttons'>
-          <button className="start-btn">START</button>
-          <button className="stop-btn">STOP</button>
-          <button className="lap-btn">LAP</button>
-          <button className="reset-btn">RESET</button>
+     <section>
+        <h1 className="seconds-elapsed">{(currentTime / 1000).toFixed(3)}</h1>
+        <section className="buttons">
+          <button className="start-btn" onClick={startTimer}>
+            START
+          </button>
+          <button className="stop-btn" onClick={stopTimer}>
+            STOP
+          </button>
+          <button className="lap-btn" onClick={lapTimer}>
+            LAP
+          </button>
+          <button className="reset-btn" onClick={resetTimer}>
+            RESET
+          </button>
         </section>
       </section>
-      <section className='lap-section'>
+      <section className={`lap-section ${laps.length > 0 ? 'visible' : ''}`}>
         <h2>Laps</h2>
-        <section className='laps'>
-          <p>lap</p>
-          <p>lap</p>
-          <p>lap</p>
-        </section>
+        {laps.length > 0 ? (
+          <section className="laps">
+            {laps.map((lap, index) => (
+              <p key={index}>Lap {index + 1}: {lap}s</p>
+            ))}
+          </section>
+        ) : (
+          <p>No Laps Recorded Yet</p>
+        )}
       </section>
     </div>
   )
